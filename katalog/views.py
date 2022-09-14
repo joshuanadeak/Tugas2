e82aa6b339f07dea1eaeb47cb27629890e49a0ec
Joshua Mihai Daniel Nadeak <joshua.mihai@ui.ac.id>
2022-09-15 02:36:56 +0700

    Pengerjaan berbagai fungsi hingga tahap akhir

--- katalog/views.py
+++ katalog/views.py
@@ -5,8 +5,8 @@ from katalog.models import CatalogItem
 def show_katalog(request):
     data_katalog = CatalogItem.objects.all()
     context = {
-    'list_barang': data_katalog,
-    'nama': 'Joshua Mihai Daniel Nadeak',
-    'NPM': '2106635285'
+        'list_katalog': data_katalog,
+        'nama': 'Joshua Mihai Daniel Nadeak',
+        'NPM': '2106635285'
     }
     return render(request, "katalog.html", context)
