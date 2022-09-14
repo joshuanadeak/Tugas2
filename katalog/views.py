76231f0f35022ee41c9e38b3b9c4f6cecea4d7af
Joshua Mihai Daniel Nadeak <joshua.mihai@ui.ac.id>
2022-09-14 23:54:31 +0700

    Fixes for the main program of Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_katalog,
+    'nama': 'Joshua Mihai Daniel Nadeak',
+    'NPM': '2106635285'
+    }
+    return render(request, "katalog.html", context)
