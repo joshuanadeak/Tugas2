a7010648074d5d82aee9a169f6cb183e76f0aeae
joshuanadeak <87121927+joshuanadeak@users.noreply.github.com>
2022-09-15 11:24:09 +0700

    At least try to make a test for bonus

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from katalog.models import CatalogItem
+from katalog.views import show_katalog
 
-# Create your tests here.
+# Credits to Daniel for the idea, because I really don't understand this before
+class KatalogTest(TestCase):
+    def test_new_catalog(self):
+        CatalogItem.objects.create(
+            item_name = "Permen Uranium", 
+            item_price = 110000000, 
+            item_stock = 3, 
+            description = "Tubuhmu bisa bernyala-nyala", 
+            rating = 5, 
+            item_url = "https://r.mtdv.me/permenuranium"
+        )
+        uraniumpermen = CatalogItem.objects.get(item_url="https://r.mtdv.me/permenuranium")
+        self.assertEquals(uraniumpermen.item_url, "https://r.mtdv.me/permenuranium")
